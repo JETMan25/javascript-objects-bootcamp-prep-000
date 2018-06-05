d8ba7b98658eeaee0d939cebae9c53c93276bf6f
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-06-05 01:32:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
+var playlist = {
+  DMB: 'Two step', 
+  Radiohead: 'creep'
+  
+}
+
 Object.assign({}, playlist, {DMB: 'two step'}, {Radiohead: 'creep'}, {STP: 'vaseline'})
 
 function updatePlaylist(){
